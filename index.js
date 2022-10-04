31ad976a0323e93ede6079725b8648b27634cfff
songbs <dogpalse@hashlab.net>
2022-10-04 19:13:23 +0900

    connect mongodb with mongoose and dotenv

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+const { PORT, MONGO_URI } = process.env;
+
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+	console.log("connected Mongo DB");
+}).catch(e => {
+	console.error(e);
+});
 
 app.listen(5000, () => {
   console.log("Start Server...!");
